aacaef66a4610b3b07d2471543170d256da8d685
LukaPrcic12345 <bobsagott17@gmail.com>
2022-01-06 09:27:03 +0100

    zapoceo home sekciju

--- main.php
+++ main.php
@@ -37,7 +37,61 @@
 
     </div>
 </header>
-<!--kraj -->
+<!-- header kraj -->
+
+<!-- home section start-->
+<section class="home" id="home">
+    <div class="home-slider">
+        <div class="wrapped">
+            <div class="slide">
+                <div class="content">
+                    <span> Our special dishes</span>
+                    <h3>Spicy noodles</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum
+                        cumque cupiditate fuga iste iusto mollitia non numquam obcaecati odit quaerat? Beatae eveniet</p>
+                    <a href="#" class="btn"></a>
+                </div>
+                <div class="image">
+                    <img src="img/nicke.jpg" alt="">
+                </div>
+
+            </div>
+
+            <div class="slide">
+                <div class="content">
+                    <span> Our special dishes</span>
+                    <h3>Fried chicken</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum
+                        cumque cupiditate fuga iste iusto mollitia non numquam obcaecati odit quaerat? Beatae eveniet</p>
+                    <a href="#" class="btn"></a>
+                </div>
+                <div class="image">
+                    <img src="img/micke.jpg" alt="">
+                </div>
+
+            </div>
+
+            <div class="slide">
+                <div class="content">
+                    <span> Our special dishes</span>
+                    <h3>Hot pizza</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum
+                        cumque cupiditate fuga iste iusto mollitia non numquam obcaecati odit quaerat? Beatae eveniet</p>
+                    <a href="#" class="btn"></a>
+                </div>
+                <div class="image">
+                    <img src="img/vico.jpg" alt="">
+                </div>
+
+            </div>
+
+
+
+        </div>
+
+    </div>
+</section>
+<!-- home section kraj -->
 
 
 
