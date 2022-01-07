bee453aac866a41acebefb2125f909e705d04b8b
LukaPrcic12345 <bobsagott17@gmail.com>
2022-01-07 15:31:20 +0100

    dodao meta tagove

--- main.php
+++ main.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Luka Prcic">
+    <meta name="keywords" content="food delivery,pizza,burger,chicken,fast">
+    <meta name="description" content="Fast food delivery website">
+
+
     <title>Los Pollos Hermanos food delivery</title>
 
     <!--font awesome cdn link -->
