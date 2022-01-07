85cd5d89ea97865967c0dcb1738f46a67a80e51b
LukaPrcic12345 <bobsagott17@gmail.com>
2022-01-07 17:30:53 +0100

    podelio main na vise delova

---
+++ home.php
@@ -0,0 +1,69 @@
+<!-- home section start-->
+<section class="home" id="home">
+    <div class="home-slider">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+            <!-- indicators --->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+                <div class="item active">
+                    <div class="slide">
+                        <div class="content">
+                            <span> Our special dishes</span>
+                            <h3>Spicy noodles</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum
+                                cumque cupiditate fuga iste iusto mollitia non numquam obcaecati odit quaerat? Beatae eveniet</p>
+                            <a href="#" class="btn">order now</a>
+                        </div>
+                        <div class="image">
+                            <img src="img/nicke.jpg" alt="">
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="slide">
+                        <div class="content">
+                            <span> Our special dishes</span>
+                            <h3>Fried chicken</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum
+                                cumque cupiditate fuga iste iusto mollitia non numquam obcaecati odit quaerat? Beatae eveniet</p>
+                            <a href="#" class="btn">order now</a>
+                        </div>
+                        <div class="image">
+                            <img src="img/micke.jpg" alt="MIHAJLO">
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="slide">
+                        <div class="content">
+                            <span> Our special dishes</span>
+                            <h3>Hot pizza</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum
+                                cumque cupiditate fuga iste iusto mollitia non numquam obcaecati odit quaerat? Beatae eveniet</p>
+                            <a href="#" class="btn">order now</a>
+                        </div>
+                        <div class="image">
+                            <img src="img/vico.jpg" alt="">
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+</section>
+<!-- home sekcija kraj -->
